a9adf5f3a148fb93d3ae8132be355259f1f78598
brian070069 <kkipngeno409@gmail.com>
2025-01-30 15:08:30 +0300

    implementation of in memory authentication

--- src/main/java/com/security/securitydemo/SecurityConfig.java
+++ src/main/java/com/security/securitydemo/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
@@ -21,4 +25,18 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}password1")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}adminPassword1")
+                .roles("ADMIN")
+                .build();
+
+       return new InMemoryUserDetailsManager(user1,admin);
+    }
+
 }
