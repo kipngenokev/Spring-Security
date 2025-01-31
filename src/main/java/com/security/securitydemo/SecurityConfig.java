627a7f7e44b666bf79330ea1a729e9f8a7a69674
brian070069 <kkipngeno409@gmail.com>
2025-01-31 15:41:08 +0300

    successful h2-database authentication

--- src/main/java/com/security/securitydemo/SecurityConfig.java
+++ src/main/java/com/security/securitydemo/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.security.securitydemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,16 +16,29 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
-        });
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//        http.formLogin(Customizer.withDefaults());
+//        http.authorizeHttpRequests((requests) -> {
+//            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.requestMatchers("/h2-console/**").permitAll()
+//                    .anyRequest()).authenticated();
+//        });
+//        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+////        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        return (SecurityFilterChain)http.build();
+        http.authorizeHttpRequests((requests)->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
+        http.sessionManagement(session
+                ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //http.formLogin(withDefaults());
         http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
     @Bean
     public UserDetailsService userDetailsService(){
